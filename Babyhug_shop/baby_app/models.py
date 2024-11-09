23736f4dc28edb295679938896c94372bbf8213f
swapnacv21 <swapnavenu116@gmail.com>
2024-11-10 01:35:59 +0530

    book_my_show_django

---
+++ Babyhug_shop/baby_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    product_id=models.TextField()
+    product_name=models.TextField()
+    price=models.IntegerField()
+    offer_price=models.IntegerField()
+    img=models.FileField()
---
+++ Babyhug_shop/baby_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from .models import *
+
+# Create your views here.
+def shop_login(req):
+    if 'shop' in req.session:
+        return redirect(shop_home)
+    else:
+        if req.method=='POST':
+            uname=req.POST['uname']
+            password=req.POST['password']
+            data=authenticate(username=uname,password=password)
+            if data:
+                login(req,data)
+                req.session['shop']=uname
+                return redirect(shop_home)
+        return render(req,'login.html')
+    
+def shop_logout(req):
+    logout(req)
+    req.session.flush()
+    return redirect(shop_login)
+
+def shop_home(req):
+    if 'shop' in req.session:
+        return render(req,'shop/shop_home.html')
+    else:
+        return redirect(shop_login)
